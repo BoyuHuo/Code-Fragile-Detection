4595db9c05f5f7043038641cbb2c6ab687d96b1c
BoyuHuo <boyu.huo.china@gmail.com>
2019-12-26 22:12:38 -0500

    move backend vul

--- src/main/java/com/ocdl/client/service/impl/FlaskSegmentService.java
+++ src/main/java/com/ocdl/client/service/impl/FlaskSegmentService.java
@@ -8,54 +8,53 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import java.io.File;
 import java.nio.file.Paths;
+
 
 @Service
 public class FlaskSegmentService implements SegmentService {
 
-  private static final Logger logger = LoggerFactory.getLogger(DefaultKafkaConsumerService.class);
+    private static final Logger logger = LoggerFactory.getLogger(DefaultKafkaConsumerService.class);
 
-  @Autowired private HttpRequestService httpRequestService;
+    @Autowired
+    private HttpRequestService httpRequestService;
 
-  @Value("${flask_sever_url}")
-  private String FLASKSEVERURL;
+    @Value("${flask_sever_url}")
+    private String FLASKSEVERURL;
 
-  @Value("${workspace.path}")
-  private String WORKSPACEPATH;
+    @Value("${workspace.path}")
+    private String WORKSPACEPATH;
 
-  @Value("${models.path}")
-  private String MODELBASEPATH;
+    @Value("${models.path}")
+    private String MODELBASEPATH;
 
-  @Value("${pictures.path}")
-  private String PICBASEPATH;
+    @Value("${pictures.path}")
+    private String PICBASEPATH;
 
-  @Value("${segmentation.path}")
-  private String SEGPICBASEPATH;
+    @Value("${segmentation.path}")
+    private String SEGPICBASEPATH;
 
-  @Value("${ground.truth.path}")
-  private String GROUNDTRUTHBASEPATH;
+    @Value("${ground.truth.path}")
+    private String GROUNDTRUTHBASEPATH;
 
-  @Override
-  public File run(String pictureName) {
+    @Override
+    public File run(String pictureName) {
 
-    String modelPath = Paths.get(WORKSPACEPATH, MODELBASEPATH, Client.currentModelName).toString();
-    String picturePath = Paths.get(WORKSPACEPATH, PICBASEPATH, pictureName).toString();
 
-    String basePictureName = pictureName.substring(0, pictureName.lastIndexOf("."));
+        String modelPath = Paths.get(WORKSPACEPATH, MODELBASEPATH, Client.currentModelName).toString();
+        String picturePath = Paths.get(WORKSPACEPATH, PICBASEPATH, pictureName).toString();
 
-    String outputPath =
-        Paths.get(WORKSPACEPATH, SEGPICBASEPATH, basePictureName + "_seg.png").toString();
-    String groundTruthPath =
-        Paths.get(WORKSPACEPATH, GROUNDTRUTHBASEPATH, basePictureName + "_segmentation.png")
-            .toString();
+        String basePictureName = pictureName.substring(0, pictureName.lastIndexOf("."));
 
-    String body =
-        String.format(
-            "{\"model_path\":\"%s\", \"test_pic_path\":\"%s\", \"output_image_path\":\"%s\", \"ground_truth_path\":\"%s\"}",
-            modelPath, picturePath, outputPath, groundTruthPath);
-    logger.info("Send request to Flask server: " + body);
-    httpRequestService.post(FLASKSEVERURL, body);
-    return new File(Paths.get(SEGPICBASEPATH, basePictureName + "_seg.png").toString());
-  }
+        String outputPath = Paths.get(WORKSPACEPATH,SEGPICBASEPATH, basePictureName + "_seg.png").toString();
+        String groundTruthPath = Paths.get(WORKSPACEPATH,GROUNDTRUTHBASEPATH, basePictureName + "_segmentation.png").toString();
+
+        String body = String.format("{\"model_path\":\"%s\", \"test_pic_path\":\"%s\", \"output_image_path\":\"%s\", \"ground_truth_path\":\"%s\"}", modelPath, picturePath, outputPath, groundTruthPath);
+        logger.info("Send request to Flask server: " + body);
+        httpRequestService.post(FLASKSEVERURL, body);
+        return new File(Paths.get(SEGPICBASEPATH, basePictureName + "_seg.png").toString());
+
+    }
 }
