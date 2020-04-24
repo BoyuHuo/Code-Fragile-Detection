3421a52e6ce3870f3a58cf1782669e2629edf577
BoyuHuo <boyu.huo.china@gmail.com>
2020-04-23 23:58:16 -0400

    add upload tags

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -7,50 +7,6 @@ function ajaxMessageReader(response, func) {
 
 }
 
-function uploadTag(){
-    var project_name = $("#project-name");
-    var git_url = $("#git-url");
-    var branch = $("#branch");
-    var code_url = $("#code-url");
-    var data = {}
-
-     var trArray = $("#result-body").find('tr').each(function(){
-        var tdArr = $(this).children();
-        var filePath = tdArr.eq(1).html();
-        var tag =tdArr.eq(4).find('input').attr('checked')?'1':0;
-        data[filePath]=tag;
-     });
-
-      $.ajax({
-            url: "/rest/vul/upload",
-            type: "POST",
-            contentType: 'application/json',
-            dataType: "json",
-            data: JSON.stringify({
-                project_name: project_name.val(),
-                git_url: git_url.val(),
-                branch: branch.val(),
-                code_url: code_url.val(),
-                data:data
-            }),
-            beforeSend: function (XMLHttpRequest) {
-                $("#Loading").show();
-            },
-            timeout: 0,
-            success: function (data) {
-                ajaxMessageReader(data, function (data) {
-                    alert("Tag Upload Successful, thank you")
-                })
-            },
-            error: function (data) {
-                alert(data.error);
-            },
-            complete: function (XMLHttpRequest, textStatus) {
-                $("#Loading").hide();
-            }
-        });
-}
-
 function codeDetect() {
     var project_name = $("#project-name");
     var git_url = $("#git-url");
@@ -84,15 +40,13 @@ function codeDetect() {
                     let tdFileName = "<td> " + fileName + "</td>";
                     let tdFilePath = "<td>" + key + "</td>";
                     let tdValue = "<td>" + value + "</td>"
-                    let tdRisk = "<td style='color:#67c23a'>NO</td>";
+                    let tdRisk = "<td>Safe</td>";
                     let value_float = parseFloat(value);
                     if (value_float == 0) {
-                        tdRisk = "<td style='color:#67c23a'>NO</td>";
+                        tdRisk = "<td>Safe</td>";
                     } else {
-                        tdRisk = "<td style='color:#dd6161'>YES</td>";
+                        tdRisk = "<td>Risky</td>";
                     }
-                    let tdTag = "<input type='checkbox' id='"+key+"' name='tag' value='0'> <label for='vehicle1'> Risky</label><br>";
-
                     tr += tdFileName;
                     tr += tdFilePath;
                     tr += tdRisk;
@@ -100,8 +54,6 @@ function codeDetect() {
                     tr += "</tr>";
 
                     resultBody.append(tr);
-
-                    $('#uploadBtn').show();
                 });
             })
         },
