15a69629b382bcced23373bc63dd13a9f78b9630
BoyuHuo <boyu.huo.china@gmail.com>
2020-04-24 00:56:42 -0400

    issue fixed

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -7,48 +7,48 @@ function ajaxMessageReader(response, func) {
 
 }
 
-function uploadTag(){
+function uploadTag() {
     var project_name = $("#project-name");
     var git_url = $("#git-url");
     var branch = $("#branch");
     var code_url = $("#code-url");
     var data = {}
 
-     var trArray = $("#result-body").find('tr').each(function(){
+    var trArray = $("#result-body").find('tr').each(function() {
         var tdArr = $(this).children();
         var filePath = tdArr.eq(1).html();
-        var tag =tdArr.eq(4).find('input').attr('checked')?'1':0;
-        data[filePath]=tag;
-     });
+        var tag = tdArr.eq(4).find('input').attr('checked') ? '1' : 0;
+        data[filePath] = tag;
+    });
 
-      $.ajax({
-            url: "/rest/vul/upload",
-            type: "POST",
-            contentType: 'application/json',
-            dataType: "json",
-            data: JSON.stringify({
-                project_name: project_name.val(),
-                git_url: git_url.val(),
-                branch: branch.val(),
-                code_url: code_url.val(),
-                data:data
-            }),
-            beforeSend: function (XMLHttpRequest) {
-                $("#Loading").show();
-            },
-            timeout: 0,
-            success: function (data) {
-                ajaxMessageReader(data, function (data) {
-                    alert("Tag Upload Successful, thank you")
-                })
-            },
-            error: function (data) {
-                alert(data.error);
-            },
-            complete: function (XMLHttpRequest, textStatus) {
-                $("#Loading").hide();
-            }
-        });
+    $.ajax({
+        url: "/rest/vul/upload",
+        type: "POST",
+        contentType: 'application/json',
+        dataType: "json",
+        data: JSON.stringify({
+            project_name: project_name.val(),
+            git_url: git_url.val(),
+            branch: branch.val(),
+            code_url: code_url.val(),
+            data: data
+        }),
+        beforeSend: function(XMLHttpRequest) {
+            $("#Loading").show();
+        },
+        timeout: 0,
+        success: function(data) {
+            ajaxMessageReader(data, function(data) {
+                alert("Tag Upload Successful, thank you")
+            })
+        },
+        error: function(data) {
+            alert(data.error);
+        },
+        complete: function(XMLHttpRequest, textStatus) {
+            $("#Loading").hide();
+        }
+    });
 }
 
 function codeDetect() {
@@ -68,16 +68,16 @@ function codeDetect() {
             branch: branch.val(),
             code_url: code_url.val()
         }),
-        beforeSend: function (XMLHttpRequest) {
+        beforeSend: function(XMLHttpRequest) {
             $("#Loading").show();
         },
         timeout: 0,
-        success: function (data) {
-            ajaxMessageReader(data, function (data) {
+        success: function(data) {
+            ajaxMessageReader(data, function(data) {
                 var resultBody = $("#result-body");
                 resultBody.empty();
 
-                $.each(data["data"], function (key, value) {
+                $.each(data["data"], function(key, value) {
                     let tr = "<tr>";
                     let keyStr = key.split("/");
                     let fileName = keyStr[keyStr.length - 1];
@@ -91,12 +91,13 @@ function codeDetect() {
                     } else {
                         tdRisk = "<td style='color:#dd6161'>YES</td>";
                     }
-                    let tdTag = "<input type='checkbox' id='"+key+"' name='tag' value='0'> <label for='vehicle1'> Risky</label><br>";
+                    let tdTag = "<input type='checkbox' id='" + key + "' name='tag' value='0'> <label for='vehicle1'> Risky</label><br>";
 
                     tr += tdFileName;
                     tr += tdFilePath;
                     tr += tdRisk;
                     tr += tdValue;
+                    tr += tdTag;
                     tr += "</tr>";
 
                     resultBody.append(tr);
@@ -105,10 +106,10 @@ function codeDetect() {
                 });
             })
         },
-        error: function (data) {
+        error: function(data) {
             alert(data.error);
         },
-        complete: function (XMLHttpRequest, textStatus) {
+        complete: function(XMLHttpRequest, textStatus) {
             $("#Loading").hide();
         }
     });
